752041c614729e322f20f850f7de952d5a191bc8
Nathan Gearing <natgearing@gmail.com>
2017-02-28 11:34:00 +1100

    Fix added check to make sure file exists

--- inc/helper-functions.php
+++ inc/helper-functions.php
@@ -8,7 +8,7 @@ function ggi_include($url)
 
     // TRUE to follow any "Location: " header that the server sends as part of the HTTP header (note this is recursive,
     //PHP will follow as many "Location: " headers that it is sent, unless CURLOPT_MAXREDIRS is set).
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+    //curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
 
     // TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -16,10 +16,12 @@ function ggi_include($url)
     // Execute the given cURL session.
     $file = curl_exec($curl);
 
+    $info = curl_getinfo($curl);
+
     // Closes a cURL session and frees all resources. The cURL handle, curl, is also deleted.
     curl_close($curl);
 
-    if ($file === false) {
+    if ($file === false || $info['http_code'] == 404) {
         return;
     }
 
